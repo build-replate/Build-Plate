db2aceced46f66e4daa77f59ad56b54fb2fd0f84
trucane <trufashionista2013@gmail.com>
2019-05-21 17:46:41 -0500

    added About page

---
+++ navigation/navigation.js
@@ -0,0 +1,34 @@
+
+
+
+// class Dropdown{
+//     constructor(element){
+
+//         this.element = element
+
+//         this.button = this.element.querySelector('.hamburger');
+
+        
+//         this.content = document.querySelector('.top-section header nav');
+        
+//         console.log(this.button)
+        
+//         this.button.addEventListener('click', () => {
+//             this.toggleMenu()
+//         });
+//     }
+
+//     toggleMenu(){
+//         this.content.classList.toggle('menu-hidden');
+//         console.log(this.content)
+//         console.log('------------------------')
+//     }
+// }
+
+// let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+
+
+
+
+
+  
