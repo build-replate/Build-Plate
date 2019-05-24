ff29d870c816c50902681952e809a5c024a6c7c5
trucane <trufashionista2013@gmail.com>
2019-05-23 21:55:23 -0500

    WIP

--- components/scroller.js
+++ components/scroller.js
@@ -17,18 +17,13 @@ class Carousel {
   
     nextImage() {
       this.nextFeed.addEventListener("click", () => {
-
         this.toggleImage(1);
-
-
-        
       });
-
     }
+
   
     previousImage() {
       this.previousFeed.addEventListener("click", () => {
-
           this.toggleImage(-1);
       });
     }
@@ -44,25 +39,24 @@ class Carousel {
 
     toggleImage(direction){
       // On "next" click... if count = total images then take count back to the beginning "0"
-      if (this.count === (box.length -1) && direction === 1){
+      if (this.count === (this.allImages.length -1) && direction === 1){
         this.count = 0;
       }else if(this.count === 0 && direction === -1){
-        this.count = box.length - 1;
+        this.count = this.allImages.length - 1;
       }else{
         this.count = this.count + direction ;
       }
 
 
-    box.forEach(card => card.classList.add("hidden"));
+      this.allImages.forEach(card => card.classList.add("hidden"));
 
-    box[this.count].classList.toggle("hidden");
+      this.allImages[this.count].classList.toggle("hidden");
     }
   }
   
   const feedback = document.querySelector(".feed-holder");
   const box = feedback.querySelectorAll(".feed-card");
   
-  // displays the first card
   document.querySelectorAll(".feed-card").forEach( card =>{
     if(card === box[0]){
 
